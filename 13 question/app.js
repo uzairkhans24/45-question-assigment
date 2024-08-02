eecaceebe8b258460c73ca4d97fa7a0397931bdb
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:50:15 +0500

    Add files via upload

---
+++ 13 question/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let Transport = ["Yamaha R1", "Kawasaki", "BMW M1000RR", "Tessla"];
+console.log(`"i would you like to own a": ${Transport[0]}`);
+console.log(`"i would you like to own a": ${Transport[1]}`);
+console.log(`"i would you like to own a": ${Transport[2]}`);
+console.log(`"i would you like to own a": ${Transport[3]}`);
---
+++ 13 question/app.ts
@@ -0,0 +1,9 @@
+// question 13 ===>Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, 
+// and make a list that stores several examples. Use your list to print a series of statements about these items,
+//  such as “I would like to own a Honda motorcycle.”
+
+let Transport: string[] = ["Yamaha R1", "Kawasaki","BMW M1000RR","Tessla"];
+console.log(`"i would you like to own a": ${Transport[0]}`);
+console.log(`"i would you like to own a": ${Transport[1]}`);
+console.log(`"i would you like to own a": ${Transport[2]}`);
+console.log(`"i would you like to own a": ${Transport[3]}`);
