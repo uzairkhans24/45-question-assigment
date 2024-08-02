e9e476efd5bdbebfc2cb77cd959cc7c976299767
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:38:36 +0500

    Add files via upload

---
+++ 2nd question/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let myname = "DUA KHAN";
+let MASSAGE = `${myname}, hello would you like to learn typescript today?`;
+console.log(MASSAGE);
---
+++ 2nd question/app.ts
@@ -0,0 +1,15 @@
+// question 2===>Personal Message: Store a person’s name in a variable,
+//  and print a message to that person. Your message should be simple, 
+//  such as, 
+// “Hello Eric, would you like to learn some Python today?”
+
+let myname: string = "Maryam Saleem";
+let MASSAGE: string = `${myname}, hello would you like to learn typescript today?`;
+console.log(MASSAGE)
+
+
+
+
+
+
+
