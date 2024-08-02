3eabdc2be900a01c63e635516c7d7946f5cf3097
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 04:30:25 +0500

    Add files via upload

---
+++ 43rd question/app.js
@@ -0,0 +1,23 @@
+// question 43====>Unchanged Magicians: Start with your work from Exercise 40. 
+// Call the function make_great() with a copy of the array of magicians’ names. 
+// Because the original array will be unchanged, return the new array and store it in a separate array.
+//  Call show_magicians() with each array to show that you have one array of the original names and one 
+//  array with the Great added to each magician’s name.
+var magicians = ["Asif", "Fahad", "Ahmed", "Arman"];
+function make_great(magicians) {
+    var greatMagicians = [];
+    magicians.forEach(function (magician) {
+        greatMagicians.push("".concat(magician, " the Great"));
+    });
+    return greatMagicians;
+}
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+var greatMagicians = make_great(magicians.slice());
+console.log("Original magicians:");
+show_magicians(magicians);
+console.log("Great magicians:");
+show_magicians(greatMagicians);
