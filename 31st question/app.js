a6add29b8995d61a140caae02184d2be6f6b7e16
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 04:02:01 +0500

    Add files via upload

---
+++ 31st question/app.js
@@ -0,0 +1,9 @@
+// question 31 ====>No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+var usernames = [];
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+}
