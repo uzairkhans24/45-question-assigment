227a876dc9b8dcd97e51d4cc0c872dd8c52a060b
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:49:35 +0500

    Add files via upload

---
+++ 12 question/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let Names = ["Maryam", "Ali", "Fahad", "Fizan", "Khakid" ];
+console.log(`Hello ${Names[0]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[1]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[2]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[3]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[4]}, would you like to learn some typescript today? `);
---
+++ 12 question/app.ts
@@ -0,0 +1,13 @@
+// question 12 ====>Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, 
+// print a message to them.
+//  The text of each message should be the same, but each message should be personalized with the person’s name.
+
+
+
+let Names:string[] = ["Maryam", "Ali", "Fahad", "Fizan", "Khakid" ];
+
+console.log(`Hello ${Names[0]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[1]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[2]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[3]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[4]}, would you like to learn some typescript today? `);
