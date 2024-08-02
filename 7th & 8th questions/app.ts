2d19f07e5d30136db8c656904a34cef66018beb6
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:40:55 +0500

    Add files via upload

---
+++ 7th & 8th questions/app.ts
@@ -0,0 +1,14 @@
+// question 7&8 ====>Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8
+// . Be sure to enclose your operations in print statements to see the results.
+
+// You should create four lines that look like this:
+
+// console.log(5 + 3)
+
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+console.log(4 + 4); // Addition
+console.log(12 - 4); // Subtraction
+console.log(2 * 4); // Multiplication
+console.log(32 / 4); // Division
