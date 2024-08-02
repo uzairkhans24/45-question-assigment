fdd5fe99419e74830333d1dc058f3edc3b49983a
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:39:55 +0500

    Add files via upload

---
+++ 5th question/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let Author = "Humaira Ahmed";
+let Quote = "A Good books is a Good Ferinds";
+console.log(`${Author}once said, ${Quote}`);
---
+++ 5th question/main.ts
@@ -0,0 +1,9 @@
+// question 5====>Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+//  Then compose your message and store it in a new variable called message. Print your message.
+
+
+import { once } from "process";
+
+let Author: string = "Humaira Ahmed";
+let Quote: string = "A Good books is a Good Ferinds";
+console.log(`${Author}once said, ${Quote}`);
