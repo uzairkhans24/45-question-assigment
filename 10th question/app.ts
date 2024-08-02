2d8d7deae552a064a89c58b5e431a3b712b3c677
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:47:08 +0500

    Add files via upload

---
+++ 10th question/app.ts
@@ -0,0 +1,20 @@
+// question19===>Adding Comments: Choose two of the programs you’ve written, 
+// and add at least one comment to each. If you don’t have anything specific to write because your
+//  programs are too simple at this point, just add your name and the current date at the top of each program file. 
+// Then write one sentence describing what the program does.
+
+
+// This is Uzair Khan
+// Dated : 25/04/2024
+
+// This program wil run simple code to Hello World!
+
+
+console.log('TypeScript is a superset of javaScript compiler..');
+
+// This is Uzair Khan
+// Dated : 25/04/2024
+
+// This program will multiplication
+
+console.log(100*100);
