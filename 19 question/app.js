e64540313eb17d8cff619e8165f5e9911dbeb3a9
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:54:18 +0500

    Add files via upload

---
+++ 19 question/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let Guest_List = ["Hina", "Fatima", "Asma", "Fiza", "Maryam"];
+// let i = 0;
+// while (i < Guest_List.length) {
+//     console.log(`Dear ${Guest_List[i]} : \nYou are invited to Dinner!\n`);
+//     i++;
+// }
+console.log(`Total number of people invited : ${Guest_List.length}`);
---
+++ 19 question/app.ts
@@ -0,0 +1,14 @@
+// question 19--->Dinner Guests: Working with one of 
+// the programs from Exercises 14 through 18, 
+// print a message indicating the number of 
+// people you are inviting to dinner.
+
+
+
+let Guest_List: string[] = ["Hina", "Fatima", "Asma", "Fiza", "Maryam"];
+// let i = 0;
+// while (i < Guest_List.length) {
+//     console.log(`Dear ${Guest_List[i]} : \nYou are invited to Dinner!\n`);
+//     i++;
+// }
+console.log(`Total number of people invited : ${Guest_List.length}`);
