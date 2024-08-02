7a4b3df1916a4fa21056e8cde2f7fc3312f1c30f
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:56:18 +0500

    Add files via upload

---
+++ 22nd question/app.ts
@@ -0,0 +1,10 @@
+//question 22--->Intentional Error: If you haven’t 
+// received an array index error in one of your programs yet, try to make one happen. 
+// Change an index in one of your programs to produce an index error.
+//  Make sure you correct the error before closing the program.
+
+let Arr = ["maryam" ,"fatima", 35, "student" ];
+console.log("produce an index error:");
+console.log(Arr[4]+`\n`);
+console.log("correction in an index error:");
+console.log(Arr[3]);
