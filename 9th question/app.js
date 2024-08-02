853ef7bf654b36973d17252c7e87f4ab770b6151
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 04:49:39 +0500

    Add files via upload

---
+++ 9th question/app.js
@@ -0,0 +1,3 @@
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favNumber = 29;
+console.log("I love this number because my birth date is: ".concat(favNumber));
---
+++ 9th question/app.ts
@@ -0,0 +1,4 @@
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favNumber : number = 29;
+console.log(`I love this number because my birth date is: ${favNumber}`)
