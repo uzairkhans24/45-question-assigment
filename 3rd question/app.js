c061f6584e83c42f108be3fc0063071a0552bd7f
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:39:05 +0500

    Add files via upload

---
+++ 3rd question/app.js
@@ -0,0 +1,6 @@
+// question3-----Name Cases: Store a person’s name in a variable, and then print that person’s
+//  name in lowercase, uppercase, and titlecase.
+var Name = "Maryam Saleem";
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
---
+++ 3rd question/app.ts
@@ -0,0 +1,8 @@
+// question3-----Name Cases: Store a person’s name in a variable, and then print that person’s
+//  name in lowercase, uppercase, and titlecase.
+
+let Name: string = "Maryam Saleem";
+
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase()); 
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
