978e17b25a6fd22e788859a6d85e4a54474ad772
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 04:27:58 +0500

    Add files via upload

---
+++ 39th question/app.js
@@ -0,0 +1,11 @@
+// question 39====>City Names: Write a function called city_country() 
+// that takes in the name of a city and its country. The function should return a
+//  string formatted like this:
+// "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+console.log(city_country("Multan", "Pakistan"));
+console.log(city_country("Sudia Arab", "Sudia Arab"));
+console.log(city_country("NewYork", "USA"));
