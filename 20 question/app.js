6c51c8150c187df53dcfd3149f9eef8df379347e
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:54:42 +0500

    Add files via upload

---
+++ 20 question/app.js
@@ -0,0 +1,16 @@
+//question  20 ---->Think of something you could store in a array. 
+// For example, you could make a list of 
+// mountains, rivers, countries, cities, languages, or anything 
+// else you’d like. Write a program that creates a list containing these items.
+var Locations = [
+    "mount everest",
+    "K2",
+    "Dubai",
+    "Sudia",
+    "Chnanab",
+    "",
+];
+console.log("\nList of Location");
+for (var i = 0; i < Locations.length; i++) {
+    console.log(Locations[i]);
+}
---
+++ 20 question/app.ts
@@ -0,0 +1,20 @@
+//question  20 ---->Think of something you could store in a array. 
+// For example, you could make a list of 
+// mountains, rivers, countries, cities, languages, or anything 
+// else you’d like. Write a program that creates a list containing these items.
+
+
+const Locations: string[] = [
+  "mount everest",
+  "K2",
+  "Dubai",
+  "Sudia",
+  "Chnanab",
+  "",
+];
+
+console.log("\nList of Location");
+
+for (let i = 0; i < Locations.length; i++) {
+  console.log(Locations[i]);
+} 
