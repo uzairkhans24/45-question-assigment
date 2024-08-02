e7e2d19dbc2bc70f868feac0e6c957cb71cbbe0b
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2024-08-03 03:39:32 +0500

    Add files via upload

---
+++ 4th questoion/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let Femous_person = "Allama Iqbal";
+let message = `${Femous_person} once said, " without power, is mere philosophy. ..."`;
+console.log(message);
+let Our_leader = "Quaid-e-Azam";
+let lesson = `${Our_leader} Once said, "work work or only work..."`;
+console.log(lesson);
---
+++ 4th questoion/app.ts
@@ -0,0 +1,14 @@
+// qestion 4th====>Famous Quote: Find a quote from a famous person you admire. 
+// Print the quote and the name of its author. Your output should look something like the following, 
+// including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let Femous_person: string= "Allama Iqbal";
+let message: string =  `${Femous_person} once said, " without power, is mere philosophy. ..."`;
+console.log(message)
+
+
+let Our_leader: string= "Quaid-e-Azam";
+let lesson: string = `${Our_leader} Once said, "work work or only work..."`;
+console.log(lesson)
